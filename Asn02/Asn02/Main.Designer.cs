d8c3901e0680e4e0fd5b6db40cd97cde2526bffe
uoi100 <uoi100x@gmail.com>
2015-09-30 21:51:10 -0700

    Renders Squares, Renders on resize

---
+++ Asn02/Asn02/Main.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Asn02
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.White;
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(284, 261);
+            this.panel.TabIndex = 1;
+            this.panel.Click += new System.EventHandler(this.PanelClick);
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.formPaint);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel);
+            this.Name = "Main";
+            this.Text = "Calvin Truong Assignment 02";
+            this.Resize += new System.EventHandler(this.formResize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ Asn02/Asn02/Main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asn02
+{
+    public partial class Main : Form
+    {
+        Rectangle[] sides;
+        int colorSwitch;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            sides = new Rectangle[2];
+            colorSwitch = 0;
+        }
+
+        private void PanelClick(object sender, EventArgs e)
+        {
+            if (colorSwitch == 0)
+                colorSwitch++;
+            else
+                colorSwitch--;
+
+            drawBackground();
+            drawRectangles();
+        }
+
+        private void drawBackground()
+        {
+            sides[0] = new Rectangle(0, 0, ClientRectangle.Width / 2, ClientRectangle.Height);
+            sides[1] = new Rectangle(ClientRectangle.Width / 2, 0, ClientRectangle.Width / 2, ClientRectangle.Height);
+
+            using (Graphics g = panel.CreateGraphics())
+            {
+                if (colorSwitch == 0)
+                {
+                    g.FillRectangle(Brushes.Black, sides[0]);
+                    g.FillRectangle(Brushes.White, sides[1]);
+                }
+                else
+                {
+                    g.FillRectangle(Brushes.White, sides[0]);
+                    g.FillRectangle(Brushes.Black, sides[1]);
+                }
+            }
+        }
+
+        private void drawRectangles()
+        {
+            Rectangle rect = new Rectangle(0, 0, 25, 25);
+            int vGap = 25;
+            int hGap = 25;
+            int rMax = ClientRectangle.Height / (rect.Height + vGap);
+            int cMax = (ClientRectangle.Width / 2) / (rect.Width + hGap);
+            int width = ClientRectangle.Width / 2;
+
+            for (int rIndex = 0; rIndex < rMax; rIndex++)
+            {
+                int y1 = rect.Height * (rIndex);
+                int vSpace = vGap * (rIndex + 1);
+
+                for (int cIndex = 0; cIndex < cMax; cIndex++)
+                {
+                    int x1 = rect.Width * (cIndex);
+                    int x2 = rect.Width * (cIndex + 1);
+                    int hSpace = hGap * (cIndex);
+
+                    // Render Rectangles
+                    using (Graphics g = panel.CreateGraphics())
+                    {
+                        g.FillRectangle(Brushes.LightGreen, new Rectangle(width - x2 - hSpace, y1 + vSpace, rect.Width, rect.Height));
+                        g.FillRectangle(Brushes.LightGreen, new Rectangle(width + x1 + hSpace, y1 + vSpace, rect.Width, rect.Height));
+                    }
+                }
+            }
+        }
+
+        private void formPaint(object sender, PaintEventArgs e)
+        {
+            drawBackground();
+            drawRectangles();
+        }
+
+        private void formResize(object sender, EventArgs e)
+        {
+            drawBackground();
+            drawRectangles();
+        }
+    }
+}
